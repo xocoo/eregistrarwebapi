ed57d170772ead7bf402ec794a1bcd50790a9eda
Khosbayar <xocoooo@gmail.com>
2022-07-08 13:47:43 -0500

    First commit

---
+++ src/main/java/edu/mum/cs/cs425/demoweapps/eregistrarwebapi/EregistrarwebapiApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demoweapps.eregistrarwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EregistrarwebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EregistrarwebapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mum/cs/cs425/demoweapps/eregistrarwebapi/EregistrarwebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demoweapps.eregistrarwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EregistrarwebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
